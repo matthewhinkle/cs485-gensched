a70016a1a7c99569a1e4056ebaf857f605542d4b
Steven Hansen <stevenhansen@n2-158-235.dhcp.drexel.edu>
2013-03-10 16:07:56 -0400

    Merging

--- src/schedule/ScheduleHeuristic.java
+++ src/schedule/ScheduleHeuristic.java
@@ -1,5 +1,8 @@
 package src.schedule;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import src.optimizer.Heuristic;
 
 public class ScheduleHeuristic implements Heuristic<Schedule> {
@@ -10,7 +13,29 @@ public class ScheduleHeuristic implements Heuristic<Schedule> {
 		for(Week w : evolvable.getWeeks()) {
 			// Everything is on Day 0 for now.
 			Day day = w.getDay(0);
+			List<String> teams = new ArrayList<String>();
+			for(NFLEvent e : day.getEvents()) {
+				teams.add(e.getAway());
+				teams.add(e.getHome());
+			}
+			
+			for(String team : teams) {
+				value += (1 - occurences(team, teams));
+			}
 		}
+		System.out.println(value);
+		return value;
+	}
+	
+	public Integer occurences(String find, List<String> list) {
+		Integer value = 0;
+		
+		for(String v : list) {
+			if(v.equals(find)) {
+				value += 1;
+			}
+		}
+		
 		return value;
 	}
 }
